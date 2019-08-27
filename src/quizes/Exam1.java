420aa014ae65ce445474e9549f229bcb64c96710
Hisham Raihan <heshow@Hishams-MacBook-Pro.local>
2019-08-27 18:27:46 -0400

    first commit

---
+++ src/quizes/Exam1.java
@@ -0,0 +1,43 @@
+package quizes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exam1 {
+
+    //create a program which will
+    // - take a specific number of student name from the console
+    // - for each student, it should ask for 4 subjects name and marks for them all
+    // - your code should get the avg marks & total for the each of the students
+    // - store all the students name, marks, subjects in individual ArrayList/Hashset and print all subjects, students name from that
+
+    //Tips : You may want to use Scanner, ArrayList, HashSet/ArrayList
+    //Your code needs to by dynamic enough to handle as many numbers of student user may want to.
+    //Think Smartly !
+
+
+    private static Scanner scanner;
+
+
+    public static void main (String[] args){
+
+        String Name;
+        String Subject;
+        int Grade;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please Enter Name");
+        Name = scanner.nextLine();
+
+        System.out.println("Please Enter Subject");
+        Subject = scanner.nextLine();
+
+
+
+    }
+
+
+
+    }
+
