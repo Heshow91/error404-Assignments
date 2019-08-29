3e969939fcb398afd9ed21300c5a78fdef37f7ed
Hisham Raihan <heshow@Hishams-MBP.home>
2019-08-28 22:43:24 -0400

    commit

---
+++ src/oopStuff/OopAbstract.java
@@ -0,0 +1,19 @@
+package oopStuff;
+
+public abstract class OopAbstract {
+
+public void fart(){
+    System.out.println("When we have to much gas");
+}
+public void smelly(){
+    System.out.println("when we fart it smells");
+}
+public void sit(){
+    System.out.println("when i sit i sometimes fart");
+}
+public void push(){
+    System.out.println("after I fart and sit,I push to excrete");
+}
+
+
+}
