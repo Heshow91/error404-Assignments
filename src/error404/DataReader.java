f2db1fcf0062c8a62af9d7ef0e3f6bd7ed91ebd7
Hisham Raihan <heshow@Hishams-MBP.home>
2019-08-28 21:45:56 -0400

    commit

--- src/error404/DataReader.java
+++ src/error404/DataReader.java
@@ -1,9 +1,30 @@
 package error404;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class DataReader {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
 
+        String text;
+        String h = null;
+
+        try {
+            FileReader fileReader = new FileReader("/Users/heshow/Desktop/JavaQuizes/src/error404/somthing.txt");
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((text = bufferedReader.readLine()) != null) {
+
+                h = text;
+            }
+            fileReader.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(h);
     }
 }
