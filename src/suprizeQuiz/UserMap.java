74c1b353144dd8f8ed4b58f9ae0ffe7f60b94231
Hisham Raihan <heshow@Hishams-MacBook-Pro.local>
2019-09-03 12:29:41 -0400

    commit

--- src/suprizeQuiz/UserMap.java
+++ src/suprizeQuiz/UserMap.java
@@ -7,10 +7,10 @@ import java.util.Map;
 public class UserMap {
     public static void main(String[] args) {
         Map<Integer,String> Map = new HashMap<>();
-        Map.put(10, "Sanna");
-        Map.put(20, "Taiz");
-        Map.put(30, "Ibn");
-        Map.put(40, "Aden");
+        Map.put(1, "Sanna");
+        Map.put(2, "Taiz");
+        Map.put(3, "Ibn");
+        Map.put(4, "Aden");
 
         Map<String, List<String>> list = new HashMap<String, List<String>>();
 
